66e8935e84d939a625c2f9f38ee180792b11b065
shema Raymond <65981217+shema-hub@users.noreply.github.com>
2025-02-10 23:40:53 +0200

    fix: update SQL script to insert a new customer into the customer table with corrected casing

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,2 @@
-USE alx_book_store;
-
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS) 
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
